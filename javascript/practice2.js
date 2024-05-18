122c29133413e49691e4909d4da7ed0c929a967b
Yash Pal <ypal23808@gmail.com>
2024-05-19 00:16:51 +0530

    object literal in js

--- javascript/practice2.js
+++ javascript/practice2.js
@@ -70,3 +70,38 @@ for (let i = 0; i <= arrs.length; i++) {
 }
 
 console.log(largest);
+
+//! Qs1. Create a program that generates a random number representing a dice roll.
+// [The number should be between 1 and 6].
+
+let randomNo = Math.floor(Math.random() * 6) + 1;
+
+console.log(randomNo);
+
+//? Qs2. Create an object representing a car that stores the following properties for the
+// car: name, model, color.
+// Print the car’s name.
+
+let car = {
+  name: "BMW",
+  model: "7 series",
+  color: "black",
+};
+
+console.log(car.name);
+
+//! Qs3. Create an object Person with their name, age and city.
+// Edit their city’s original value to change it to “New York”.
+// Add a new property country and set it to the United States.
+
+let Person = {
+  name: "shradha",
+  age: 21,
+  city: "delhi",
+};
+
+Person.city = "New York";
+
+Person.country = "United States";
+
+console.table(Person);
