7de8f85333d23c18e4f1eedb3b551d623d5e76d9
Yash Pal <ypal23808@gmail.com>
2024-05-20 20:38:43 +0530

    concat all string of array

--- javascript/function.js
+++ javascript/function.js
@@ -3,14 +3,15 @@ function sum(a,b){
     console.log(result);
 }
 
-sum(2,4);
+// sum(2,4);
+
 
 function average(a,b,c){
     let avg = (a+b+c)/3;
     console.log(avg);
 }
 
-average(2,5,2);
+// average(2,5,2);
 
 function table(n){
     for(let i=n;i<=n*10;i+=n){
@@ -18,11 +19,27 @@ function table(n){
     }
 }
 
-table(2);
+// table(2);
 
 function subtract(a,b){
     return a - b;
 }
 
-let result = subtract(5,2);
-console.log(result);
+// let result = subtract(5,2);
+// let ans = subtract((5,2),2);
+
+// console.log(result);
+// console.log(ans);
+
+let str = ["hello", "world", " !"];
+
+function concat(str){
+    let value = "";
+
+    for (let i = 0; i < str.length; i++) {
+        value += str[i];
+    }
+    return value;
+}
+
+console.log(concat(str));
