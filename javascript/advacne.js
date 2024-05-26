cc726ce7c781edc8f451d5d8d4ba96e5a04cf9ab
Yash Pal <ypal23808@gmail.com>
2024-05-26 23:20:10 +0530

    async and await

--- javascript/advacne.js
+++ javascript/advacne.js
@@ -25,6 +25,19 @@ function changeColor(color, delay) {
   });
 }
 
+async function demo() {
+  try {
+    await changeColor("red", 1000);
+    await changeColor("green", 1000);
+    await changeColor("blue", 1000);
+    await changeColor("aqua", 1000);
+    changeColor("maroon", 1000);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+/*
 changeColor("red", 1000)
   .then(() => {
     console.log("red color changed");
@@ -40,43 +53,46 @@ changeColor("red", 1000)
   })
   .catch(() => {
     console.log("no color changed");
-  });
+  }); */
 
 //? callback nesting -> callback hell
 
-// changeColor("red",1000,()=>{
-//     changeColor("green",1000,()=>{
-//         changeColor("yellow",1000,()=>{
-//             changeColor("aqua",1000);
-//         });
-//     });
-// })
+/*
+changeColor("red",1000,()=>{
+    changeColor("green",1000,()=>{
+        changeColor("yellow",1000,()=>{
+            changeColor("aqua",1000);
+        });
+    });
+}); */
 
 //? Promises
+/*
+function saveDb(data) {
+  return new Promise((resolve, reject) => {
+    let speed = Math.floor(Math.random() * 10) + 1;
+    if (speed > 4) {
+      resolve("Data saved successfully");
+    } else {
+      reject("Weak connection detected");
+    }
+  });
+}
+*/
 
-// function saveDb(data) {
-//   return new Promise((resolve, reject) => {
-//     let speed = Math.floor(Math.random() * 10) + 1;
-//     if (speed > 4) {
-//       resolve("Data saved successfully");
-//     } else {
-//       reject("Weak connection detected");
-//     }
-//   });
-// }
-
-// saveDb("Hello")
-//   .then((res) => {
-//     console.log("data1 saved successfully");
-//     console.log("result = ", res);
-//     return saveDb("hello world");
-//   })
-//   .then((res) => {
-//     console.log("data2 saved successfully");
-//     console.log("result = ", res);
-//   })
-//   .catch(() => {
-//     console.log("Promise rejected");
-//   });
+/*
+saveDb("Hello")
+  .then((res) => {
+    console.log("data1 saved successfully");
+    console.log("result = ", res);
+    return saveDb("hello world");
+  })
+  .then((res) => {
+    console.log("data2 saved successfully");
+    console.log("result = ", res);
+  })
+  .catch(() => {
+    console.log("Promise rejected");
+  }); */
 
 // then() & catch() - > js object
