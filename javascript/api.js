482adb1fb63e3ed3fc8d20a75c47a71c0ebd7fa9
Yash Pal <ypal23808@gmail.com>
2024-05-27 21:46:52 +0530

    API - fetch data using axios and display data

--- javascript/api.js
+++ javascript/api.js
@@ -5,15 +5,15 @@ let res = JSON.parse(jsonData);
 console.log(res);
 
 let student = {
-    name : "Nansh",
-    age : 21
-}
+  name: "Nansh",
+  age: 21,
+};
 
 // objeect to string
 let ans = JSON.stringify(student);
 console.log(ans);
 
-let url = "https://catfact.ninja/fact";
+// let url = "https://catfact.ninja/fact";
 
 // fetch(url).then((res) => {
 //     console.log(res);
@@ -27,14 +27,82 @@ let url = "https://catfact.ninja/fact";
 //     console.log(err);
 // });
 
-async function getData(){
-    try {
-        let res = await fetch(url);
-        let data = await data.json();
-        console.log(data.fact);
-    } catch (error) {
-        console.log("Error : ",error);
-    }
+// async function getData(){
+//     try {
+//         let res = await fetch(url);
+//         let data = await data.json();
+//         console.log(data.fact);
+//     } catch (error) {
+//         console.log("Error : ",error);
+//     }
 
-    console.log("End..");
+//     console.log("End..");
+// }
+
+// async function getData(){
+//     try {
+//         let res = await axios.get(url);
+//         console.log(res.fact);
+//     } catch (error) {
+//         console.log("Error : ",error);
+//     }
+
+// }
+
+/*
+let url2 = "https://dog.ceo/api/breeds/image/random";
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async () => {
+  let data = await getImage();
+  console.log(data);
+
+  let img = document.querySelector("#result");
+  img.setAttribute("src", data);
+});
+
+async function getImage() {
+  try {
+    let res = await axios.get(url2);
+    console.log(res);
+    return res.data.message;
+  } catch (error) {
+    return "NO Image Found";
+  }
+}
+
+*/
+
+let url3 = "http://universities.hipolabs.com/search?name=";
+
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click", async () => {
+  let country = document.querySelector("input").value;
+  let data = await getColleges(country);
+  console.log(data);
+  showColleges(data);
+});
+
+function showColleges(college) {
+    let list = document.querySelector("#list");
+    list.innerText = "";
+  for (col of college) {
+    console.log(col.name);
+    let li = document.createElement("li");
+    li.innerText = col.name;
+    list.appendChild(li);
+  }
+}
+
+async function getColleges() {
+  try {
+    let res = await axios.get(url3 + country);
+    console.log(res);
+    return res.data;
+  } catch (error) {
+    console.log("error", error);
+    return [];
+  }
 }
