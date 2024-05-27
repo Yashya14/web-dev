022b9204e16fc832e45cbbce728f0db658950357
Yash Pal <ypal23808@gmail.com>
2024-05-27 20:19:25 +0530

    async fetch

--- javascript/api.js
+++ javascript/api.js
@@ -13,3 +13,28 @@ let student = {
 let ans = JSON.stringify(student);
 console.log(ans);
 
+let url = "https://catfact.ninja/fact";
+
+// fetch(url).then((res) => {
+//     console.log(res);
+//     return res.json();
+// })
+// .then((data) => {
+//     console.log(data.fact);
+//     return fetch(url)
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
+
+async function getData(){
+    try {
+        let res = await fetch(url);
+        let data = await data.json();
+        console.log(data.fact);
+    } catch (error) {
+        console.log("Error : ",error);
+    }
+
+    console.log("End..");
+}
