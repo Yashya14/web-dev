460cf26fbb4599371d701e9957480086b3f52ed5
Yash Pal <ypal23808@gmail.com>
2024-05-29 21:30:02 +0530

    ejs - conditional statements, loops and icludes

--- Express/index.js
+++ Express/index.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 let port = 8080;
 
-app.get('/', (req, res) => {
-    res.send("Welcome to Home Page!");
+app.set("views", path.join(__dirname,"/views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  let dicevalue = Math.floor(Math.random() * 6) + 1;
+  res.render("home.ejs",{value:dicevalue});
 });
 
-app.get('/apple', (req, res) => {
-    res.send("Welcome to apple!");
-});
+let followers = ["John", "shyam", "rajat"];
+app.get("/ig/:username", (req, res) => {
+  let {username} = req.params;
+  res.render("instagram.ejs",{username,followers});
+})
+
+// app.get('/', (req, res) => {
+//     res.send("Welcome to Home Page!");
+// });
+
+// app.get('/apple', (req, res) => {
+//     res.send("Welcome to apple!");
+// });
 
 // app.get('*', (req, res) => {
 //     res.send("page does not exist");
@@ -19,23 +34,23 @@ app.get('/apple', (req, res) => {
 //     res.send("this is post request");
 // });
 
-app.get("/:username/:id", (req, res) => {
-    let {username, id} = req.params;
-    console.log(username);
-    res.send(`Welcome ${username}! to CodeDev`);
-});
+// app.get("/:username/:id", (req, res) => {
+//     let {username, id} = req.params;
+//     console.log(username);
+//     res.send(`Welcome ${username}! to CodeDev`);
+// });
 
-app.get("/search", (req, res) => {
-    let {q} = req.query;
-    if(!q){
-        res.send("no search query");
-        console.log("no search query");
-    }
-    res.send(`<h1>Search results for query : ${q}</h1>`);
-});
+// app.get("/search", (req, res) => {
+//     let {q} = req.query;
+//     if(!q){
+//         res.send("no search query");
+//         console.log("no search query");
+//     }
+//     res.send(`<h1>Search results for query : ${q}</h1>`);
+// });
 
 app.listen(port, () => {
-  console.log(`app listening on ${port}`);
+  console.log(`app listening on http://localhost:${port}`);
 });
 
 // app.use((req, res) => {
